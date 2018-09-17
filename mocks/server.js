53f5ab7094494bb2a644eb5383ebf021ff7ec0e4
Mark Faga <mark.faga@ezcater.com>
2018-09-16 23:00:45 -0400

    Add new schema

--- mocks/server.js
+++ mocks/server.js
@@ -2,7 +2,31 @@ const {ApolloServer, gql} = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    user(id: ID!): User
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    favoriteFoods: [FavoriteFoodItem]
+  }
+
+  type FavoriteFoodItem {
+    id: ID!
+    foodItem: FoodItem!
+    user: User!
+    eatingFrequency: EatingFrequency!
+  }
+
+  type FoodItem {
+    id: ID!
+    name: String!
+  }
+
+  enum EatingFrequency {
+    DAILY
+    WEEKLY
+    MONTHLY
   }
 `;
 
