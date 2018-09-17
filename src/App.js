cfb1d04debf1136a7a9ab708776fd368d39a54e6
Mark Faga <mark.faga@ezcater.com>
2018-09-16 22:08:13 -0400

    Add Apollo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const client = new ApolloClient({
+  uri: 'http://localhost:3001',
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  </ApolloProvider>
+);
 
 export default App;
