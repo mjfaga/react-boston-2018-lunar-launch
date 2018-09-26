91eedd543a82d13b303da2c3754fccf04857d2d8
Mark Faga <mark.faga@ezcater.com>
2018-09-26 19:10:01 -0400

    Add comments to integration tests for readability

--- cypress/integration/AddFavoriteFoodToUser.spec.js
+++ cypress/integration/AddFavoriteFoodToUser.spec.js
@@ -10,7 +10,11 @@ describe('Adding Favorite Foods to a User', () => {
         }),
       }),
     });
+
+    // Navigate to page
     cy.visit('http://localhost:3000/user/1');
+
+    // Validate page content
     cy.get('h2').contains('favorite foods:');
     cy.get('li').should('have.length', 2);
     cy.contains('li', 'I like to eat Spaghetti on a weekly basis');
@@ -19,16 +23,32 @@ describe('Adding Favorite Foods to a User', () => {
 
   describe('when the user does not fill in the Food Name', () => {
     it('shows an error', () => {
+      // Navigate to page
       cy.visit('http://localhost:3000/user/1');
+
+      // Fill in only Eating Frequency
+      cy.get('select').select('DAILY');
+
+      // Submit form
       cy.get('button').click();
+
+      // Validate error message
       cy.contains('div', 'Food Name and Eating Frequency are required!');
     });
   });
 
   describe('when the user does not select an Eating Frequency', () => {
     it('shows an error', () => {
+      // Navigate to page
       cy.visit('http://localhost:3000/user/1');
+
+      // Fill in only Food Name
+      cy.get('input').type('Fluffernutter Sandwich');
+
+      // Submit form
       cy.get('button').click();
+
+      // Validate error message
       cy.contains('div', 'Food Name and Eating Frequency are required!');
     });
   });
@@ -50,17 +70,29 @@ describe('Adding Favorite Foods to a User', () => {
           }),
         }),
       });
+
+      // Navigate to page
       cy.visit('http://localhost:3000/user/1');
+
+      // Validate page content
       cy.contains('div', 'No favorites yet!');
+
+      // Fill in and submit form (triggers mutation)
       cy.get('input').type('Fluffernutter Sandwich');
       cy.get('select').select('DAILY');
       cy.get('button').click();
+
+      // Validate that mutation result displays on screen
       cy.contains(
         'li',
         'I like to eat Fluffernutter Sandwich on a daily basis'
       );
+
+      // Validate form is reset
       cy.get('input').should('have.value', '');
       cy.get('select').should('have.value', null);
+
+      // Validate success message
       cy.contains('div', 'You did it!');
     });
   });
