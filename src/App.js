49cc1aed021cadc24714d7c09b635c85325f657b
Mark Faga <mfaga@wayfair.com>
2019-07-02 09:56:54 -0400

    Remove Apollo from App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from 'react-apollo';
 import {BrowserRouter} from 'react-router-dom';
 import AppHeader from './AppHeader';
 import AppBody from './AppBody';
 import './App.css';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
-});
-
 const App = () => (
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <div>
-        <AppHeader />
-        <AppBody />
-      </div>
-    </BrowserRouter>
-  </ApolloProvider>
+  <BrowserRouter>
+    <div>
+      <AppHeader />
+      <AppBody />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
